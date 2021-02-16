6e4bb547063fa3246f74b85fc6179cd9dda0b4ff
cristian <preduscacristian@gmail.com>
2021-02-16 20:42:52 +0200

    upload

---
+++ precache-manifest.dd6e953d9fd51be93c3f59bf62d17919.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5663912a2d498d2b406a04b302c885b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "179c0dc6ba3034e440b4",
+    "url": "/static/css/main.19a2a1c6.chunk.css"
+  },
+  {
+    "revision": "cb5d79a9c87d36c7309a",
+    "url": "/static/js/2.b4c9b855.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.b4c9b855.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "179c0dc6ba3034e440b4",
+    "url": "/static/js/main.161c4769.chunk.js"
+  },
+  {
+    "revision": "f6de843015c4d59733e9",
+    "url": "/static/js/runtime-main.16cf4fbd.js"
+  }
+]);
